20293e2711c86f3166597c24aee7228e04c74bb3
Quentin Machu <me@quentin-machu.fr>
2016-03-23 16:16:36 -0400

    Rename consts and add source

--- jwt/noncestorage/local/local.go
+++ jwt/noncestorage/local/local.go
@@ -25,10 +25,10 @@ import (
 )
 
 const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	nonceBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/"
+	nonceIdxBits = 6                   // 6 bits to represent a nonce index
+	nonceIdxMask = 1<<nonceIdxBits - 1 // All 1-bits, as many as nonceIdxBits
+	nonceIdxMax  = 63 / nonceIdxBits   // # of nonce indices fitting in 63 bits
 )
 
 func init() {
@@ -81,18 +81,18 @@ func (ln *Local) Generate() (string, error) {
 	}
 }
 
+// https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
 func randStringBytesMask(randSource rand.Source, n int) string {
 	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, randSource.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, randSource.Int63(), nonceIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = randSource.Int63(), letterIdxMax
+			cache, remain = randSource.Int63(), nonceIdxMax
 		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
+		if idx := int(cache & nonceIdxMask); idx < len(nonceBytes) {
+			b[i] = nonceBytes[idx]
 			i--
 		}
-		cache >>= letterIdxBits
+		cache >>= nonceIdxBits
 		remain--
 	}
 	return string(b)
