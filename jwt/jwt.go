quay/jwtproxy

commit 6d07e41fee83265fd96c326a05b9ffc28df0f3df

Remove the duplicate kid from the claims, already in headers

jwt/jwt.go
@@ -48,7 +48,6 @@ func init() {
 func Sign(req *http.Request, key *key.PrivateKey, params config.SignerParams) error {
 	// Create Claims.
 	claims := jose.Claims{
-		"kid": key.ID(),
 		"iss": params.Issuer,
 		"aud": req.URL.Scheme + "://" + req.URL.Host,
 		"iat": time.Now().Unix(),

commit 065c8b7898bfacabb15bcb3babddddf33c18fb31

Fix "kid" bug introduced by 6d07e41

jwt/jwt.go
@@ -88,10 +88,9 @@ func Verify(req *http.Request, keyServer keyserver.Reader, nonceVerifier noncest
 
 	// Verify claims.
 	now := time.Now().UTC()
-
-	kid, exists, err := claims.StringClaim("kid")
-	if !exists || err != nil {
-		return nil, errors.New("Missing or invalid 'kid' claim")
+	kid, exists := jwt.Header["kid"]
+	if !exists {
+		return nil, errors.New("Missing 'kid' claim")
 	}
 	iss, exists, err := claims.StringClaim("iss")
 	if !exists || err != nil {