6df4a14c90c60a064455f98e79b111f49fb3f2ca
Jimmy Zelinskie <jimmyzelinskie@gmail.com>
2016-04-26 15:17:27 -0400

    *: add missing copyright headers

--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -1,3 +1,17 @@
+// Copyright 2016 CoreOS, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package jwt
 
 import (
@@ -10,11 +24,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/coreos/jwtproxy/config"
-	"github.com/coreos/jwtproxy/stop"
 	"github.com/coreos/go-oidc/jose"
 	"github.com/coreos/go-oidc/key"
 	"github.com/coreos/go-oidc/oidc"
+	"github.com/coreos/jwtproxy/config"
+	"github.com/coreos/jwtproxy/stop"
 	"github.com/stretchr/testify/assert"
 )
 
